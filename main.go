2b0aa352616d5a6badaf57ac79af9b916fb360e4
rvldodo <nono.qwerty28@gmail.com>
2024-03-18 18:46:32 +0700

    add redis as db

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 
 	"github.com/rvldodo/chi-basic/appllication"
 )
@@ -10,7 +12,10 @@ import (
 func main() {
 	app := appllication.New()
 
-	err := app.Start(context.TODO())
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	err := app.Start(ctx)
 	if err != nil {
 		fmt.Println("Failed start the server:", err)
 	}
